cfe6304c2e2df1f6414a1eb1b085ac0a9b82ff2a
patonz <patonz91@gmail.com>
2015-01-30 00:25:07 +0100

    Resources changes

--- src/builder/model/ResourcesHandler.java
+++ src/builder/model/ResourcesHandler.java
@@ -7,45 +7,53 @@ import java.util.ArrayList;
 /**
  * Created by Leonardo on 14/01/2015.
  */
-public  class ResourcesHandler extends BuildersHandler {
+public class ResourcesHandler extends BuildersHandler {
 
 
     public ArrayList<Resource> resources;
 
 
-
-
-    public ResourcesHandler(ArrayList resources, Double weight){
+    public ResourcesHandler(ArrayList resources, Double weight) {
         this.resources = resources;
         this.weight = weight;
     }
 
 
-
     @Override
     public Object Similarity(Object obj) {
-        if(!(obj instanceof ResourcesHandler)){
+        if (!(obj instanceof ResourcesHandler)) {
             throw new SimilarTypeNotFoundException();
         }
 
-        int check = 0;
-        double totalcheck = resources.size();
 
-        for(Resource resA : resources){
-            for(Resource resB : ((ResourcesHandler) obj).resources){
-                if((int)resA.Similarity(resB)==1){
-                    check +=  (int) resA.Similarity(resB);
+        ArrayList<Resource> min;
+        ArrayList<Resource> max;
+        if (resources.size() >= ((ResourcesHandler) obj).resources.size()) {
+            min = ((ResourcesHandler) obj).resources;
+            max = resources;
+        } else {
+            min = resources;
+            max = ((ResourcesHandler) obj).resources;
+        }
+
+
+        int check = 0;
+        double totalcheck = max.size();
+
+        for (Resource resA : min) {
+            for (Resource resB : max) {
+                if ((int) resA.Similarity(resB) == 1) {
+                    check += (int) resA.Similarity(resB);
                     break;
                 }
 
             }
         }
 
-        Double grade = (check * 100)/ totalcheck;
+        Double grade = (check * 100) / totalcheck;
 
-        return (!Double.isNaN(grade))? grade : 0.0;
+        return (!Double.isNaN(grade)) ? grade : 0.0;
     }
-
 
 
 }
